6bfe5a9b9490408888c6acdb1a4f744a43a569a9
shaff600 <shafyl@live.co.uk>
2022-01-22 18:28:07 +0000

    created product details page

--- SneakerGame/settings.py
+++ SneakerGame/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -128,7 +129,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
