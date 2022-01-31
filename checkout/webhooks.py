41c08804310135ee9d927b2bdc9a2103118d8b28
shaff600 <shafyl@live.co.uk>
2022-01-31 17:51:09 +0000

    added countries to checkout form

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -27,11 +27,14 @@ def webhook(request):
         )
     except ValueError as e:
         # Invalid payload
+        print("Error 1")
         return HttpResponse(content=e, status=400)
     except stripe.error.SignatureVerificationError as e:
         # Invalid signature
+        print("Error 2")
         return HttpResponse(content=e, status=400)
     except Exception as e:
+        print("Error 3")
         return HttpResponse(content=e, status=400)
 
     # Set up a webhook handler
