4195b6ec8be6b6ad85e46de8ca6911365fd0187a
shaff600 <shafyl@live.co.uk>
2022-02-04 11:10:41 +0000

    debug true, lowercase

--- SneakerGame/settings.py
+++ SneakerGame/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = TRUE
+DEBUG = True
 
 ALLOWED_HOSTS = ['sneakergame.herokuapp.com', 'localhost']
 
