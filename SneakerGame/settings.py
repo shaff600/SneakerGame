e860a0d3eb57cdad1d8bf5f80a83f2a058bf84e2
shaff600 <shafyl@live.co.uk>
2022-02-04 11:08:25 +0000

    debug true

--- SneakerGame/settings.py
+++ SneakerGame/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = TRUE
 
 ALLOWED_HOSTS = ['sneakergame.herokuapp.com', 'localhost']
 
